cf7044aa5a9b8e8c327762096930b9f922876d62
jadensoto <66880395+jadensoto@users.noreply.github.com>
2020-06-13 12:43:45 -0700

    Update buton.js

--- buton.js
+++ buton.js
@@ -1,3 +1,8 @@
-function myFunction(elmnt,clr) {
-  elmnt.style.color = clr;
+function changeColor(elmnt,clr) {
+  if (elmnt.style.color == clr) {
+    elmnt.style.color = 'red';
+  } 
+  else {
+    elmnt.style.color = clr;
+  }
 }
